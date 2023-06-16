5fcd2b4f6517261409689b2c1eca431783f566f6
sarb <sarbnagra1@gmail.com>
2023-06-16 12:50:43 +0530

    working login and signup front and backend

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,11 @@
 /* eslint-disable no-undef */
 const express = require("express");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+
 const User = require("../models/User.js");
 
 const router = express.Router();
 
-// User login route
 router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -23,13 +22,7 @@ router.post("/login", async (req, res) => {
       return res.status(401).json({ message: "Invalid email or password" });
     }
 
-    // Generate a JWT token
-    const token = jwt.sign(
-      { userId: user._id, email: user.email, isAdmin: user.isAdmin },
-      process.env.JWT_SECRET
-    );
-
-    return res.status(200).json({ token });
+    return res.status(200).json({ message: "Login successful" });
   } catch (error) {
     console.error("Error logging in:", error);
     return res.status(500).json({ message: "Internal server error" });
